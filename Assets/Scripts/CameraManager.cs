cfd4e32502a65fb7ad30b367042496b598b98cb7
Mushroom29 <kevinbjohnson9@gmail.com>
2017-11-14 22:03:04 -0600

    Code Cleanup Before Merge

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour {
 
     private GameObject mainCamera;
-    //private int TilePixelLength = 32;
 
     public void CameraSetup(List<List<TileProperty.ThingsOnTile>> boardArray)
     {        
@@ -20,25 +19,13 @@ public class CameraManager : MonoBehaviour {
         float newXPosition = 0f;
         float newYPosition = 0f;
 
-        print(boardWidth / boardHeight);
-        print(screenWidth / screenHeight);
-
-        // 2 ways we need to determine the camera by whatever dimension is the limiting factor
         // Compare the aspect ratio of the game board to the game display to determine tile resolution
         if((boardWidth / boardHeight) > (screenWidth / screenHeight))
         {
             // The board width is limited so it will determine tile resolution
-            //screenWidth / boardWidth
-
-            //   (screenWidth / screenHeight ) * boardHeight
-
-            //newOrthographicSize = ((screenWidth / screenHeight) * (boardWidth / (boardHeight * (float)2))); // 4.32
-            //newOrthographicSize = (((boardHeight / (float)2) * (boardWidth / (boardHeight))) / (screenWidth / screenHeight));
             newOrthographicSize = ((boardWidth / (float) 2) * (screenHeight / screenWidth));
             newYPosition = ((boardHeight / (float)2) - (float)0.5);
             newXPosition = ((boardWidth / (float)2) - (float)0.5);
-
-            print("Width fill");
         }
         // 
         else
@@ -47,28 +34,12 @@ public class CameraManager : MonoBehaviour {
             newOrthographicSize = (boardHeight / (float)2);
             newYPosition = (newOrthographicSize - (float)0.5);
             newXPosition = ((boardWidth / (float)2) - (float)0.5);
-
-            print("Height fill");
         }
 
         mainCamera.transform.position = new Vector3(newXPosition, newYPosition, -10f);
         mainCamera.GetComponent<Camera>().orthographicSize = newOrthographicSize;
 
-        print(boardWidth + " -> " + newOrthographicSize);
-        print(boardHeight + " -> " + (screenWidth / screenHeight));
-        
-        print(screenWidth);
-        print(screenHeight);
-
         //The orthographic size expresses how many world units are contained in the top half of the camera projection.
         //For example, if you set an orthographic size of 5, then the vertical extents of the viewport will contain exactly 10 units of world space.
-
-        // Po x - 3
-        // Po y - 5.5
-        // Size - 6
-        // Ti x - 7
-        // Ti y - 12
-        // Di x - 475
-        // Di y - 514
     }
 }
