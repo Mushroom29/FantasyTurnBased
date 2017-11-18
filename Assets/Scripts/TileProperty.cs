d511375f54a7558d2b18fbdb2b79a9d47f41a92c
Mushroom29 <Mushroom29@users.noreply.github.com>
2017-11-18 17:19:23 -0600

    Added Human and Orc on Game Start

--- Assets/Scripts/TileProperty.cs
+++ Assets/Scripts/TileProperty.cs
@@ -7,12 +7,14 @@ public class TileProperty
 {
     public enum TileType { Grass, Hill };
     public enum ResourceType { None, Mineral, Sheep };
+    public enum UnitType { None, Human, Orc };
 
     public class ThingsOnTile
     {
         public Vector3 vectorLocation;
         public TileType tileType;
         public ResourceType resourceType;
+        public UnitType unitType;
 
         public ThingsOnTile(Vector3 loc, TileType tile, ResourceType res)
         {
@@ -21,11 +23,20 @@ public class TileProperty
             resourceType = res;
         }
 
+        public ThingsOnTile(Vector3 loc, TileType tile, ResourceType res, UnitType unit)
+        {
+            vectorLocation = loc;
+            tileType = tile;
+            resourceType = res;
+            unitType = unit;
+        }
+
         public ThingsOnTile()
         {
             vectorLocation = new Vector3(0, 0, 0f);
             tileType = TileProperty.TileType.Grass;
             resourceType = TileProperty.ResourceType.None;
+            unitType = TileProperty.UnitType.None;
         }
     }
 }
